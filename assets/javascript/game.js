a838e4871775a03f005c3c6c781333daae0d5f29
Michael Gelber <mpgelber7495@gmail.com>
2019-11-04 20:55:37 -0500

    Completed functionality of guessing and added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,24 @@
 // JSON structure for the data below
 characters = [
   {
-    name: "99 Red Balloons",
-    Description: "Gold Finger"
+    name: "Red Balloons",
+    Description: "Gold Finger",
+    link: "https://open.spotify.com/embed/track/0rOTMSSa6yscfOrGnzfheM"
   },
   {
     name: "My Own Worst Enemy",
-    Description: "Kitty Kitty"
+    Description: "Kitty Kitty",
+    link: "https://open.spotify.com/embed/track/33iv3wnGMrrDugd7GBso1z"
   },
   {
-    name: "I Want You Back",
-    Description: "Definitely sassy"
+    name: "Mr Brightside",
+    Description: "Kitty Kitty",
+    link: "https://open.spotify.com/embed/track/7oK9VyNzrYvRFo7nQEYkWN"
   },
   {
-    name: "Whats My Age Again",
-    Description: "BSG, Mose"
-  },
-  {
-    name: "Sweet Home Alabama",
-    Description: "Art"
-  },
-  {
-    name: "Basket Case",
-    Description: "Really great with numbers"
-  },
-  {
-    name: "Andy",
-    Description: "Ivy league"
-  },
-  {
-    name: "Toby",
-    Description: "Michael's favorite... not"
-  },
-  {
-    name: "Erin",
-    Description: "Pams follow up"
-  },
-  {
-    name: "Creed",
-    Description: "Probably the sketchiest"
+    name: "All The Small Things",
+    Description: "Kitty Kitty",
+    link: "https://open.spotify.com/embed/track/2m1hi0nfMR9vdGC8UcrnwU"
   }
 ];
 // End data
@@ -74,15 +54,11 @@ validKeys = [
   " "
 ];
 
+// create variables
 var wins = 0;
 var losses = 0;
-// var remainingGuesses;
-// var lettersGuessed;
-// var lettersGuessedCorrect;
-// var randomCharacter;
-// var randomCharacterName;
-// var lettersInCharacter;
 
+// create function that will allow us to refresh variables
 function instantiateVariables() {
   remainingGuesses = 8;
   lettersGuessed = [];
@@ -90,16 +66,19 @@ function instantiateVariables() {
   randomCharacter = characters[Math.floor(Math.random() * characters.length)];
   randomCharacterName = randomCharacter["name"].toLowerCase();
   lettersInCharacter = randomCharacterName.split("");
+  lettersGuessedCorrectCount = 0;
   return [
     remainingGuesses,
     lettersGuessed,
     lettersGuessedCorrect,
     randomCharacter,
     randomCharacterName,
-    lettersInCharacter
+    lettersInCharacter,
+    lettersGuessedCorrectCount
   ];
 }
 
+// run the instantiation function and assign the variable
 var instant = instantiateVariables();
 var remainingGuesses = instant[0];
 var lettersGuessed = instant[1];
@@ -107,9 +86,12 @@ var lettersGuessedCorrect = instant[2];
 var randomCharacter = instant[3];
 var randomCharacterName = instant[4];
 var lettersInCharacter = instant[5];
+var lettersGuessedCorrectCount = instant[6];
 
+// Refresh the remaining guesses value on the DOM
 document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
 
+// Function to insert a correctly guessed letter into the DOM
 function resetCharacter() {
   document.getElementById("guessing-holder").innerHTML = "";
   for (i = 0; i < lettersInCharacter.length; i++) {
@@ -121,41 +103,69 @@ function resetCharacter() {
   }
 }
 
+// First reset function to start off the game
 resetCharacter();
 
+// Event listener to pickup when a key is pressed (and therefore a guess is being made)
 document.addEventListener("keydown", function(event) {
   for (i = 0; i < lettersInCharacter.length; i++) {
+    // check to see if 1. the guessed letter's in the character 2. the key being pressed is valid 3. the letter hasn't already been guessed correctly, which would've allowed for hacking
     if (
       lettersInCharacter[i] === event["key"] &&
-      validKeys.includes(event["key"])
+      validKeys.includes(event["key"]) &&
+      lettersGuessedCorrect.includes(event["key"]) === false
     ) {
-      lettersGuessedCorrect += event["key"];
+      console.log(event["key"]);
+      // inserting the correctly guessed letter into the DOM
       var elementID = "letter-" + i;
       document.getElementById(elementID).innerHTML = event["key"];
-      if (lettersGuessedCorrect.length === lettersInCharacter.length) {
-        wins++;
-        document.getElementById("wins").innerHTML = wins;
-        instant = instantiateVariables();
-        remainingGuesses = instant[0];
-        lettersGuessed = instant[1];
-        lettersGuessedCorrect = instant[2];
-        randomCharacter = instant[3];
-        randomCharacterName = instant[4];
-        lettersInCharacter = instant[5];
-        resetCharacter();
-      }
+      lettersGuessedCorrectCount++;
     }
   }
+  // Add the correctly guessed character to the array so that it can't be counted again
+  if (
+    lettersInCharacter.includes(event["key"]) &&
+    validKeys.includes(event["key"])
+  ) {
+    lettersGuessedCorrect += event["key"];
+  }
+  // check to see if the song has been fully guessed correctly by comparing the length of character to the number of letters correctly guessed
+  if (lettersGuessedCorrectCount === lettersInCharacter.length) {
+    wins++;
 
+    // code to embed spotify iframe
+    songIframe =
+      '<div class="row"><div class="col-12 d-flex justify-content-center py-2"><iframe src="' +
+      randomCharacter.link +
+      '" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> </div></div>';
+    document.getElementById("spotify-holder").innerHTML += songIframe;
+
+    // Reset all of the relevant variables for a new round
+    document.getElementById("wins").innerHTML = wins;
+    instant = instantiateVariables();
+    remainingGuesses = instant[0];
+    lettersGuessed = instant[1];
+    lettersGuessedCorrect = instant[2];
+    randomCharacter = instant[3];
+    randomCharacterName = instant[4];
+    lettersInCharacter = instant[5];
+    lettersGuessedCorrectCount = instant[6];
+    resetCharacter();
+  }
+  // If a letter is guessed incorrect
   if (
     lettersGuessed.includes(event["key"]) === false &&
     lettersInCharacter.includes(event["key"]) === false &&
     validKeys.includes(event["key"])
   ) {
+    // add it to the letters guessed array
     lettersGuessed.push(event["key"]);
+    // Display this in the DOM
     document.getElementById("letters-guessed").innerHTML = lettersGuessed;
+    // Decrease remaining guesses
     remainingGuesses--;
     document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
+    // cause loss of game if the remaining guesses are zero
     if (remainingGuesses === 0) {
       losses++;
       document.getElementById("losses").innerHTML = losses;
@@ -166,6 +176,7 @@ document.addEventListener("keydown", function(event) {
       randomCharacter = instant[3];
       randomCharacterName = instant[4];
       lettersInCharacter = instant[5];
+      lettersGuessedCorrectCount = instant[6];
       resetCharacter();
     }
   }
