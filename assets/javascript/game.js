17544bdb31da0d2a5fdb7841c12afb6f05d87e30
Michael Gelber <mpgelber7495@gmail.com>
2019-11-06 18:41:08 -0500

    Fixed loss/win bug and re-styled bumper sticker

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,35 @@
 // JSON structure for the data below
 var characters = [
-  {
-    name: "Red Balloons",
-    artist: "Gold Finger",
-    link: "https://open.spotify.com/embed/track/0rOTMSSa6yscfOrGnzfheM"
-  },
-  {
-    name: "My Own Worst Enemy",
-    artist: "Lit",
-    link: "https://open.spotify.com/embed/track/33iv3wnGMrrDugd7GBso1z"
-  },
-  {
-    name: "Mr Brightside",
-    artist: "The Killers",
-    link: "https://open.spotify.com/embed/track/7oK9VyNzrYvRFo7nQEYkWN"
-  },
-  {
-    name: "All The Small Things",
-    artist: "Blink 182",
-    link: "https://open.spotify.com/embed/track/2m1hi0nfMR9vdGC8UcrnwU"
-  },
-  {
-    name: "Basket Case",
-    artist: "Green Day",
-    link: "https://open.spotify.com/embed/track/6L89mwZXSOwYl76YXfX13s"
-  },
-  {
-    name: "American Girl",
-    artist: "Tom Petty",
-    link: "https://open.spotify.com/embed/track/7MRyJPksH3G2cXHN8UKYzP"
-  },
+  // {
+  //   name: "Red Balloons",
+  //   artist: "Gold Finger",
+  //   link: "https://open.spotify.com/embed/track/0rOTMSSa6yscfOrGnzfheM"
+  // },
+  // {
+  //   name: "My Own Worst Enemy",
+  //   artist: "Lit",
+  //   link: "https://open.spotify.com/embed/track/33iv3wnGMrrDugd7GBso1z"
+  // },
+  // {
+  //   name: "Mr Brightside",
+  //   artist: "The Killers",
+  //   link: "https://open.spotify.com/embed/track/7oK9VyNzrYvRFo7nQEYkWN"
+  // },
+  // {
+  //   name: "All The Small Things",
+  //   artist: "Blink 182",
+  //   link: "https://open.spotify.com/embed/track/2m1hi0nfMR9vdGC8UcrnwU"
+  // },
+  // {
+  //   name: "Basket Case",
+  //   artist: "Green Day",
+  //   link: "https://open.spotify.com/embed/track/6L89mwZXSOwYl76YXfX13s"
+  // },
+  // {
+  //   name: "American Girl",
+  //   artist: "Tom Petty",
+  //   link: "https://open.spotify.com/embed/track/7MRyJPksH3G2cXHN8UKYzP"
+  // },
   {
     name: "The Middle",
     artist: "Jimmy Eat World",
@@ -103,7 +103,6 @@ var lettersInCharacter = instant[5];
 var lettersGuessedCorrectCount = instant[6];
 
 // Refresh the remaining guesses value on the DOM
-document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
 
 // Function to insert a correctly guessed letter into the DOM
 function resetCharacter() {
@@ -115,6 +114,9 @@ function resetCharacter() {
       i +
       "'>_</span>";
   }
+  document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
+  document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
+  document.getElementById("letters-guessed").innerHTML = lettersGuessed;
 }
 
 // Function to sleep that will be used for animating the image spin
@@ -150,59 +152,65 @@ resetCharacter();
 
 // Event listener to pickup when a key is pressed (and therefore a guess is being made)
 document.addEventListener("keydown", function(event) {
-  for (i = 0; i < lettersInCharacter.length; i++) {
-    // check to see if 1. the guessed letter's in the character 2. the key being pressed is valid 3. the letter hasn't already been guessed correctly, which would've allowed for hacking
-    if (
-      lettersInCharacter[i] === event["key"] &&
-      validKeys.includes(event["key"]) &&
-      lettersGuessedCorrect.includes(event["key"]) === false
-    ) {
-      // inserting the correctly guessed letter into the DOM
-      var elementID = "letter-" + i;
-      document.getElementById(elementID).innerHTML = event["key"];
-      lettersGuessedCorrectCount++;
+  if (
+    remainingGuesses > 0 &&
+    lettersGuessedCorrectCount !== lettersInCharacter.length
+  ) {
+    for (i = 0; i < lettersInCharacter.length; i++) {
+      // check to see if 1. the guessed letter's in the character 2. the key being pressed is valid 3. the letter hasn't already been guessed correctly, which would've allowed for hacking
+      if (
+        lettersInCharacter[i] === event["key"] &&
+        validKeys.includes(event["key"]) &&
+        lettersGuessedCorrect.includes(event["key"]) === false
+      ) {
+        // inserting the correctly guessed letter into the DOM
+        var elementID = "letter-" + i;
+        document.getElementById(elementID).innerHTML = event["key"];
+        lettersGuessedCorrectCount++;
+      }
     }
-  }
-  // Add the correctly guessed character to the array so that it can't be counted again
-  if (
-    lettersInCharacter.includes(event["key"]) &&
-    validKeys.includes(event["key"])
-  ) {
-    lettersGuessedCorrect += event["key"];
-  }
-  // check to see if the song has been fully guessed correctly by comparing the length of character to the number of letters correctly guessed
-  if (lettersGuessedCorrectCount === lettersInCharacter.length) {
-    wins++;
+    // Add the correctly guessed character to the array so that it can't be counted again
+    if (
+      lettersInCharacter.includes(event["key"]) &&
+      validKeys.includes(event["key"])
+    ) {
+      lettersGuessedCorrect += event["key"];
+    }
+    // check to see if the song has been fully guessed correctly by comparing the length of character to the number of letters correctly guessed
+    if (lettersGuessedCorrectCount === lettersInCharacter.length) {
+      wins++;
 
-    // code to embed spotify iframe
-    songIframe =
-      '<div class="row"><div class="col-12 d-flex justify-content-center py-2"><iframe src="' +
-      randomCharacter.link +
-      '" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> </div></div>';
-    document.getElementById("spotify-holder").innerHTML += songIframe;
+      // code to embed spotify iframe
+      songIframe =
+        '<div class="row"><div class="col-12 d-flex justify-content-center py-2"><iframe src="' +
+        randomCharacter.link +
+        '" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> </div></div>';
+      document.getElementById("spotify-holder").innerHTML += songIframe;
 
-    nextButtonDisplay();
-    // display the new wins value
-    document.getElementById("wins").innerHTML = wins;
-  }
-  // If a letter is guessed incorrect
-  if (
-    lettersGuessed.includes(event["key"]) === false &&
-    lettersInCharacter.includes(event["key"]) === false &&
-    validKeys.includes(event["key"])
-  ) {
-    // add it to the letters guessed array
-    lettersGuessed.push(event["key"]);
-    // Display this in the DOM
-    document.getElementById("letters-guessed").innerHTML = lettersGuessed;
-    // Decrease remaining guesses
-    remainingGuesses--;
-    document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
-    // cause loss of game if the remaining guesses are zero
-    if (remainingGuesses === 0) {
-      losses++;
       nextButtonDisplay();
-      document.getElementById("losses").innerHTML = losses;
+      // display the new wins value
+      document.getElementById("wins").innerHTML = wins;
+    }
+
+    // If a letter is guessed incorrect
+    if (
+      lettersGuessed.includes(event["key"]) === false &&
+      lettersInCharacter.includes(event["key"]) === false &&
+      validKeys.includes(event["key"])
+    ) {
+      // add it to the letters guessed array
+      lettersGuessed.push(event["key"]);
+      // Display this in the DOM
+      document.getElementById("letters-guessed").innerHTML = lettersGuessed;
+      // Decrease remaining guesses
+      remainingGuesses--;
+      document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
+      // cause loss of game if the remaining guesses are zero
+      if (remainingGuesses === 0) {
+        losses++;
+        nextButtonDisplay();
+        document.getElementById("losses").innerHTML = losses;
+      }
     }
   }
 });
